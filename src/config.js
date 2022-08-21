fc963885e61dfe563019638fb78e2097cbbe9060
Garrett Gordon <gagordon@mit.edu>
2022-08-21 19:38:56 -0400

    production

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const DEVELOPMENT = false
 
 export const BACKEND = "https://sidechain-backend.herokuapp.com"
 
-export const FRONTEND = DEVELOPMENT ? "http://localhost:3000" : "https://sidechain-frontend.herokuapp.com"
+export const FRONTEND = DEVELOPMENT ? "http://localhost:3000" : "https://www.side-chain.xyz"
 
 export const SIDECHAIN_UPLOAD_ENDPOINT = BACKEND + "/upload"
 
